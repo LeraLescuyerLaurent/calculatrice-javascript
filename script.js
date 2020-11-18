ccb5d7e4c3db6c2e073534a3dbcb59e792a2c255
lera lescuyer <lllcommunication@gmx.fr>
2020-11-19 00:31:50 +0100

    creation du html et style css    coté js initialisation de chaque constante

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButton = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearAllButton = document.querySelector('[data-all-clear]');
+const equalButton = document.querySelector('[data-equal]');
+const PreviousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
+
